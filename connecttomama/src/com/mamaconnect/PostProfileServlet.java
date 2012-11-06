34b9668b4bc7250aa64ea5d8fb59a4ad87b76cb1
Armand Li <adarcorerlreth@gmail.com>
2012-11-06 13:59:56 -0500

    and now nothing worked! woot!

--- connecttomama/src/com/mamaconnect/PostProfileServlet.java
+++ connecttomama/src/com/mamaconnect/PostProfileServlet.java
@@ -1,19 +1,27 @@
 package com.mamaconnect;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.log.Log;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
+import java.util.logging.Logger;
+
 
 @SuppressWarnings("serial")
 public class PostProfileServlet extends HttpServlet {
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String mamaName = req.getParameter("name");
 		String mamaStory = req.getParameter("story");
 		String mamaPic = req.getParameter("profilepic");
@@ -26,6 +34,6 @@ public class PostProfileServlet extends HttpServlet {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		datastore.put(profileInfo);
 		
-		
+		//resp.sendRedirect("/hello.jsp");
 	}
 }
